95876700e51006310dab3e6b51ad73d8054f576a
Tim Shaker <tim@tmshkr.com>
2020-03-27 08:59:25 -0700

    Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Button, FormGroup, Label, Spinner } from "reactstrap";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+function LoginForm(props) {
+  const [loading, setLoading] = useState(false);
+  const { history } = props;
+  const { handleSubmit, register, errors, setError } = useForm();
+
+  const onSubmit = values => {
+    setLoading(true);
+    axios
+      .post("http://localhost:5000/api/login", values)
+      .then(({ data }) => {
+        localStorage.setItem("token", data.payload);
+        history.push("/bubbles");
+      })
+      .catch(err => {
+        setLoading(false);
+        setError("response", "response", err.response.data.error);
+        console.dir(err);
+      });
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+    <div className="login">
+      <form className="form auth-form" onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup>
+          <Label for="username">Username</Label>
+          <input
+            className="form-control"
+            name="username"
+            type="username"
+            id="username"
+            ref={register({
+              required: "Required"
+            })}
+          />
+          <span className="error">{errors.email && errors.email.message}</span>
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            id="password"
+            ref={register({
+              required: "Required"
+            })}
+          />
+          <span className="error">
+            {errors.password && errors.password.message}
+          </span>
+        </FormGroup>
 
-export default Login;
+        {!loading && (
+          <>
+            <Button type="submit" color="primary" size="lg" block>
+              Login
+            </Button>
+            <span className="error">
+              {errors.response && errors.response.message}
+            </span>
+          </>
+        )}
+      </form>
+      {loading && <Spinner color="primary" />}
+    </div>
+  );
+}
+
+export default LoginForm;
